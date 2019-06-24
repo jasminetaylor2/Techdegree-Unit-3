517d4b52568b6da76c0f4807e229119d24c238f4
jasminetaylor2 <51241456+jasminetaylor2@users.noreply.github.com>
2019-06-24 16:13:52 -0400

    update js code
    jquery code

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+$("#name").focus();      //set focus on the input text element with id name when page is  loaded
+
+$("#other-title").hide(); //selects input element with id other-title and hides it
+                          //displays input element if javascript is no longer working
+
+//$("#design option").hide();
