d0fd402d7a8f673d01d6e7803dc3d6fae768cf90
jasminetaylor2 <51241456+jasminetaylor2@users.noreply.github.com>
2019-06-28 10:55:11 -0400

    design theme selected
    changes the color option to match...doesnt work to heart js

--- js/script.js
+++ js/script.js
@@ -8,36 +8,62 @@ $("#name").on('keypress', function (e) {
 
 $("#other-title").hide();             //selects input element with id other-title and hides it
 $("select#title").on('click', function () {     // displays the hidden  input field when other is clicked
- if (this.value =="other") {
-    $("input:hidden").show();
- } else { $("#other-title").hide();}            //hides the input field if not selected
-});                                        
+    if (this.value == "other") {
+        $("input:hidden").show();
+    } else { $("#other-title").hide(); }            //hides the input field if not selected
+});
 
 
 //T-Shirt Section 
 
- $("#design > option:nth-child(1)").hide();     //hides the select theme option
- const $colorOption = $('<option selected value="none">Please select a T-Shirt Theme</option>'); //variabe that hold the text value
- $("#color > option:nth-child(1)").before($colorOption); //displays the variable text to the 1st option item
+$("#design > option:nth-child(1)").hide();     //hides the select theme option
+const $colorOption = $('<option selected value="none">Please select a T-Shirt Theme</option>'); //variabe that hold the text value
+$("#color > option:nth-child(1)").before($colorOption); //displays the variable text to the 1st option item
 
- //$("#color").prepend($colorOption).val(0);         //.html hide the options but places text at top. This works but top works better
-$colorOption.css({backgroundColor: "yellow"});       //this highlight the bg of the text 
+//$("#color").prepend($colorOption).val(0);         //.html hide the options but places text at top. This works but top works better
+$colorOption.css({ backgroundColor: "yellow" });       //this highlight the bg of the text 
 
 
-$("select[name=user_design").change(function(){
-$("#color > option").each(function (index, value){
-    $(this).toggle(); //"#colors-js-puns"
-if( this.value !== " js puns") 
-{// $(value).attr('cornflowerblue');
-     $("#color > option:nth-child(2)").show();
-   $("#color > option:nth-child(3):hidden").show();
-   $("#color > option:nth-child(4):hidden").show();
-} else {$("#colors-js-puns").hide();}
+$("select[name=user_design").change(function () {         //calls the select element that holds thsirt design themes function to
+    $("#color > option").each(function (index, value) {      //selects each child option nested in color id and creates
+        $(this).toggle();                      // function to toggle between hide and show 
+        if (this.value !== " js puns")                          // 
+        {
+            $("#color > option:nth-child(2)").show();            //
+            $("#color > option:nth-child(3)").show();            //
+            $("#color > option:nth-child(4)").show();            //
+        } else
+            if (this.value !== " heart js ") {
+                $("#color > option:nth-child(5)").show();
+                $("#color > option:nth-child(6)").show();
+                $("#color > option:nth-child(7)").show();
+            }                        // $("#colors-js-puns").hide();
 
+        //this works --not includeing heart js
+
+        // if ( this.value !== " heart js ") 
+        // {$("#color > option:nth-child(5)").show();
+        //  $("#color > option:nth-child(6)").show();
+        //  $("#color > option:nth-child(7)").show(); 
+        // } else { $("#colors-js-puns").hide(); }
+
+    });
 });
+//});
 
 
-});
+// $("select[name=user_design").change(function(){
+//     $("#color > option").each(function(index, value){
+// $(this).toggle();
+// if ( this.value !== "js puns") {
+//     $("#color > option:nth-child(2)").show();
+// } if (this.value !== "heart js") {
+//     $("#color > option:nth-child(6)").show();
+// } else {$("colors-js-puns").hide(); }
+
+//     });
+
+//      });
 
 //this works
 //  $("select[name=user_design").change(function(){
@@ -45,22 +71,12 @@ if( this.value !== " js puns")
 //  if ($('select[name=user_design] option:selected').val() =='js puns')
 //     { $("div:hidden").show();  }               //("select[value~=cornflowerblue]").show$('div:hidden').show();
 //     else { }
-    
-    
+
+
 // if ($('select[name=user_design] option:selected').val() == 'heart js') {
 //         $("#colors-js-puns").show();
 
 // }
 
-// });
-
-
-
-
-// $("#color").hide();
-
-//$("#target").prop("selectedIndex", 0).change();
-// $('#design').on('change', function(){
-
-
+// });    
 
